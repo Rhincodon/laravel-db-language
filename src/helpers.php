50e239d096e8a565151aaf12023bd1ba63b663a0
WhaleShark <rhinodontypicus@gmail.com>
2015-06-15 21:47:52 +0300

    Fix

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,11 @@
 <?php
 
 if (!function_exists('db_language')) {
-    function db_language()
+    function db_language($constantName = null)
     {
+        if (trim($constantName) !== '') {
+            return app('db.language')->get($constantName);
+        }
         return app('db.language');
     }
 }
