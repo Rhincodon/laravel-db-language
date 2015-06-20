c5b58e554a259867e249b930a49e159b426c3723
WhaleShark <rhinodontypicus@gmail.com>
2015-06-20 21:17:06 +0300

    Fix

--- src/database/migrations/2015_01_01_000000_create_language_tables.php
+++ src/database/migrations/2015_01_01_000000_create_language_tables.php
@@ -31,15 +31,14 @@ class CreateLanguageTables extends Migration
 
         Schema::create('language_constant_values', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('constant_id')->unsigned();
-            $table->integer('language_id')->unsigned();
+            $table->integer('constant_id')->unsigned()->index();
+            $table->integer('language_id')->unsigned()->index();
 
             $table->foreign('constant_id')->references('id')->on('language_constants')
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('language_id')->references('id')->on('languages')
                 ->onUpdate('cascade')->onDelete('cascade');
 
-            $table->primary(['constant_id', 'language_id']);
             $table->string('value');
         });
     }
