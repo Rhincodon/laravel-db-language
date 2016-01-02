09ec54236c93b9523d411033e952d810f257301f
WhaleShark <rhinodontypicus@gmail.com>
2016-01-02 14:55:01 +0300

    Fix

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,7 @@
 if (!function_exists('db_language')) {
     function db_language($constantName = null, $constantValue = null)
     {
-        if (trim($constantName) !== '' && trim($constantValue) !== '') { // Only for default language
+        if (trim($constantName) !== '' && $constantValue !== '') { // Only for default language
             return app('db.language')->getAndSetForFirstLanguage($constantName, $constantValue);
         }
         if (trim($constantName) !== '') {
